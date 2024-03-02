99c2ece68b4d0504ef226838e6eb282c0984cca9
dawit2123 <dawitzewdu2123@gmail.com>
2024-03-02 11:29:30 +0300

    Error connecting to MongoDB Could not connect to any servers in your MongoDB Atlas cluster. One common reason is that you're trying to access the database from an IP that isn't whitelisted. Make sure your current IP address is on your Atlas cluster's IP whitelist: https://www.mongodb.com/docs/atlas/security-whitelist/

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(process.env.MONGO_DB_URI);
+    const result = await mongoose.connect(process.env.MONGO_DB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB", error.message);
