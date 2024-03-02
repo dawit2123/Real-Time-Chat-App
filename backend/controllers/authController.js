44c8ff70146e4c68d9cc721890d7a06602a3430a
dawit2123 <dawitzewdu2123@gmail.com>
2024-03-02 10:00:16 +0300

    real time chat app building process finished

---
+++ backend/controllers/authController.js
@@ -0,0 +1,6 @@
+export const signup = (req, res, next) => {
+  res.send("Sign up");
+};
+
+export const login = (req, res, next) => {};
+export const logout = (req, res, next) => {};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,35 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-const app = express();
-dotenv.config({ path: "config.env" });
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
-const PORT = process.env.PORT;
-console.log(PORT);
-app.get("/", (req, res, next) => {
-  res.send("Hello world and I am back with Dave.");
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
